b859ae8e9e914b755b97ddbcade5c41ddb53de16
ChebetPhoebe <phoebechebet4@gmail.com>
2026-03-04 15:13:23 +0300

    Clean up supabaseClient and index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// Remove this line: import storage from './storage';
-// Remove this line: window.storage = storage;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
